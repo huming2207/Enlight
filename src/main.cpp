387cebdef3cde9f01408d11639aa903bcf416047
Jackson Ming Hu <huming2207@gmail.com>
2018-01-17 18:11:55 +0800

    key: add full key support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "Service.h"
+#include "Color.h"
 
 #include <Arduino.h>
 
@@ -8,7 +9,34 @@ CFastLED fastLED; // NOLINT
 Preferences preferences; // NOLINT
 
 volatile uint8_t fastLedBright = 0;
-volatile uint16_t colorTemp = 1000;
+volatile bool keyPressed = false;
+volatile bool shieldTouched = false;
+
+void keyLightTask(void *param)
+{
+  log_i("KeyLightTask: task created.");
+
+  bool lastState;
+
+  // Endless loop starts
+  while(true) {
+
+    lastState = fastLED.getBrightness() > 0;
+
+    // If the light is on, write the maximum duty value
+    if(lastState) {
+      ledcWrite(ENLIGHT_MAIN_KEY_LEDC_CHANNEL, 8192);
+
+    } else { // Or, let it turns off slowly in 8 seconds
+      for(uint32_t ledDuty = 8192; ledDuty < 1; ledDuty -= 1) {
+        ledcWrite(ENLIGHT_MAIN_KEY_LEDC_CHANNEL, ledDuty);
+        delay(1);
+      }
+    }
+
+    delay(5);
+  }
+}
 
 void setup()
 {
@@ -26,6 +54,7 @@ void setup()
   // Setup main key's LED
   ledcSetup(ENLIGHT_MAIN_KEY_LEDC_CHANNEL, ENLIGHT_MAIN_KEY_LEDC_FREQ, ENLIGHT_MAIN_KEY_LEDC_BIT);
   ledcAttachPin(ENLIGHT_MAIN_KEY_INTR_PIN, ENLIGHT_MAIN_KEY_LEDC_CHANNEL);
+  xTaskCreatePinnedToCore(keyLightTask, "keyLightTask", 10000, nullptr, 1, nullptr, ARDUINO_RUNNING_CORE);
 }
 
 
@@ -35,19 +64,17 @@ void loop()
 
   // Sometimes interrupt does not work with FastLED (ref: https://github.com/FastLED/FastLED/wiki/Interrupt-problems)
   // As a result, use a separate task instead.
-  bool shieldTouched = (touchRead(ENLIGHT_TOUCH_INTR_PIN) <= ENLIGHT_TOUCH_INTR_SENSE);
-  bool keyPressed = (digitalRead(ENLIGHT_MAIN_KEY_INTR_PIN) == HIGH);
+  shieldTouched = (touchRead(ENLIGHT_TOUCH_INTR_PIN) <= ENLIGHT_TOUCH_INTR_SENSE);
+  keyPressed = (digitalRead(ENLIGHT_MAIN_KEY_INTR_PIN) == HIGH);
 
-  if(shieldTouched && keyPressed) {
+  // If shield touched, then adjust the brightness
+  if(shieldTouched) {
 
-
-
-  } else if(shieldTouched) {
-
+    // Get current LED brightness
     fastLedBright = fastLED.getBrightness();
 
     if(fastLedBright >= 255) {
-      fastLedBright = 0;
+      fastLedBright = 1;
     }
 
     fastLedBright += 1;
@@ -55,19 +82,18 @@ void loop()
     fastLED.setBrightness(fastLedBright);
     fastLED.show();
 
-
+  // If key pressed, then switch it off.
   } else if(keyPressed) {
 
-    // Actually 7000k is enough for daily uses.
-    if(colorTemp >= 7000) {
-      colorTemp = 1000;
+    if(fastLED.getBrightness() != 0) {
+      fastLED.setBrightness((uint8_t)preferences.getUInt(ENLIGHT_NVRAM_LED_BRIGHTNESS, 255));
     } else {
-
+      fastLED.setBrightness(0);
     }
-
-
 
   }
 
+  // Don't stress the CPU (waste CPU time)
   delay(5);
 }
+
